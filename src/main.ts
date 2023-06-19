eda0b6350f93521c875e15e5effc72278fe0dc4e
Theo Cranmore <theo.cranmore@gmail.com>
2023-06-19 20:02:18 +0100

    empty aws-exports file. import that instead.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import * as core from '@actions/core'
-import * as fs from 'fs'
 import {
   getTenantByBranchQuery,
   createTenantQuery,
   updateTenantQuery
 } from './appsyncQuery'
 
+const awsExports = require('./aws-exports').default
+
 async function run(): Promise<void> {
   try {
-    const awsExportsFilepath: string = core.getInput('awsExportsFilepath')
-    // read the contents of the file
-    const awsExportsFile = fs.readFileSync(awsExportsFilepath, 'utf8')
     const branchName: string = core.getInput('branch')
     const tenantData = await getTenantByBranchQuery({branch: branchName})
+    const awsExportsFile = JSON.stringify(awsExports)
     if (tenantData) {
       await updateTenantQuery({
         id: tenantData.id,
